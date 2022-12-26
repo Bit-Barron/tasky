b6317962f1176342cea4f8b67ff308e0a38fb18e
Bit-Barron <1bit.baron@gmail.com>
2022-12-26 18:55:03 +0100

    start with tray

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const electron = require('electron');
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Tray } = electron;
 
 let mainWindow;
 
 app.on('ready', () => {
-    mainWindow = new BrowserWindow({})
-    mainWindow.loadURL(`file://${__dirname}/src/index.html`)
+  mainWindow = new BrowserWindow({
+    height: 500,
+    width: 300,
+    frame: false,
+    resizable: false
+  });
+  mainWindow.loadURL(`file://${__dirname}/src/index.html`);
 
-})
+  new Tray();
+});
