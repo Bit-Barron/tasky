0aa1cc7d1cc9a3b42c095099fc49235406051647
Bit-Barron <1bit.baron@gmail.com>
2022-12-26 20:27:53 +0100

    fix index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const electron = require('electron');
 const { app, BrowserWindow, Tray } = electron;
 const path = require('path');
-
 let mainWindow;
-let tray;
+// let tray;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
@@ -20,11 +19,23 @@ app.on('ready', () => {
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
 
   tray = new Tray(iconPath);
+  const { height, width } = mainWindow.getBounds();
 
-  if (mainWindow.isVisible()) {
-    mainWindow.hide();
-  } else {
-   x
-    mainWindow.show();
-  }
+  tray.on('click', (event, bounds) => {
+    const { x, y } = bounds;
+    const { height, width } = mainWindow.getBounds();
+
+    if (mainWindow.isVisible()) {
+      mainWindow.hide();
+    } else {
+      const yPosition = process.platform === 'darwin' ? y : y - height;
+      mainWindow.setBounds({
+        x: Math.floor(x - width / 2),
+        y: Math.floor(yPosition),
+        height: height,
+        width: width,
+      });
+      mainWindow.show();
+    }
+  });
 });
