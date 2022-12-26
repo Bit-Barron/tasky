e2d95bb8362781d30f2371392fe52f092a850b00
Bit-Barron <1bit.baron@gmail.com>
2022-12-26 19:25:02 +0100

    asd

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const electron = require('electron');
 const { app, BrowserWindow, Tray } = electron;
 const path = require('path');
+const { workerData } = require('worker_threads');
 
 let mainWindow;
 let tray;
@@ -20,10 +21,20 @@ app.on('ready', () => {
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
 
   tray = new Tray(iconPath);
-  tray.on('click', () => {
+
+  tray.on('click', (event, bounds) => {
     if (mainWindow.isVisible()) {
       mainWindow.hide();
     } else {
+      const { x, y } = bounds;
+      const { height, width } = mainWindow.getBounds();
+      const yPosition = process.platform === 'darwin' ? y : y - height;
+      mainWindow.setBounds({
+        x: Math.floor(x - width / 2),
+        y: Math.floor(yPosition),
+        width: width,
+        height: height,
+      });
       mainWindow.show();
     }
   });
